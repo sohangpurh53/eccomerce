73b816ca0d35d53591d9fd868061265a153e24ae
Hakimuddin <sohangpurhakimuddin@gmail.com>
2023-08-27 17:44:56 +0530

    code of update 27 august

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -126,7 +126,8 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
